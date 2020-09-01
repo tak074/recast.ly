6540e7dd289268df3403422412346b536217f05c
owegami <busimeiru@gmail.com>
2020-09-01 11:35:41 -0700

    "Import videolist, videoPlayer, and searchbar to App"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import VideoList from './VideoList.js';
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
 
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em><Search /></em> </h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em><VideoPlayer /></em></h5></div>
       </div>
       <div id="videoList" className="col-md-5">
-        <div><h5><em><VideoList /></em> view goes here</h5></div>
+        <div><h5><em><VideoList /></em></h5></div>
       </div>
     </div>
   </div>
